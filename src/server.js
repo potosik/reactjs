7261ca60d6ba6e8551f9817803fc915bfe6ea44b
Artsiom Patotski <apatotski@scnsoft.com>
2017-01-19 12:35:23 +0300

    Flux-redux counter

--- src/server.js
+++ src/server.js
@@ -1,18 +1,16 @@
-import App      from 'components/App';
-
-
-
-
-
 import express  from 'express';
 import React    from 'react';
 import ReactDom from 'react-dom/server';
 import { match, RouterContext } from 'react-router';
+import { Provider } from 'react-redux';
+import configureStore from './redux/configureStore';
 import routes from './routes';
 
 const app = express();
 
 app.use((req, res) => {
+  const store = configureStore();
+
   match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
     if (redirectLocation) {
       return res.redirect(301, redirectLocation.pathName + redirectLocation.search);
@@ -26,7 +24,11 @@ app.use((req, res) => {
       return res.status(404).send('Not found');
     }
 
-    const componentHTML = ReactDom.renderToString(<RouterContext {...renderProps} />);
+    const componentHTML = ReactDom.renderToString(
+      <Provider store={store}>
+        <RouterContext {...renderProps} />
+      </Provider>
+    );
     return res.end(renderHTML(componentHTML));
   });
 });
